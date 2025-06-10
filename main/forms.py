69a091935bc8c0969f4ed95578e07f48efb954d9
Adrian Obregon <adrianlolobre@gmail.com>
2025-06-10 04:21:52 -0600

    v0.1.5 - Mejora de Front

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from main.models import User, Suggestion, HelpMessage
 
+# Formulario de registro de usuario
 class CustomUserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label="Contraseña",widget=forms.PasswordInput(attrs={'minlength': 8}))
     password2 = forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput(attrs={'minlength': 8}))
@@ -9,6 +10,7 @@ class CustomUserCreationForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+    # Se verifica si el correo electrónico ya está en uso y si las contraseñas coinciden
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email__iexact=email).exists():
@@ -29,20 +31,20 @@ class CustomUserCreationForm(forms.ModelForm):
             user.save()
         return user
     
-
+# Formulario de Contacto
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, label="Nombre")
     email = forms.EmailField(label="Correo electrónico")
     subject = forms.CharField(max_length=150, required=False, label="Asunto")
     message = forms.CharField(widget=forms.Textarea, label="Mensaje")
 
-    
+# Formulario de sugerencias/buzón
 class SuggestionForm(forms.ModelForm):
     class Meta:
         model = Suggestion
         fields = ['subject','message']
 
-
+# Formulario de chat de ayuda
 class HelpMessageForm(forms.ModelForm):
     class Meta:
         model = HelpMessage
